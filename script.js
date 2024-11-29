1614e063b2aad71c47439d754ed7a50586d9fa74
Denisse706 <acubens537@gmail.com>
2024-11-28 22:47:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este código se ejecutará cuando la página esté cargada 
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Bienvenido a la plataforma de recursos educativos gratuitos!");
+});
